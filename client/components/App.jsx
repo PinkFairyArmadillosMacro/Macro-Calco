3531a479186288c0b91cd716a42915c9486528a7
Raymond Ferrer <rnferrer@ucdavis.edu>
2023-03-04 11:55:15 -0800

    starting to build front end
    Co-authored-by: Matt Kymn mkymn10@g.ucla.edu

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Cookies from "js-cookie";
-
+import Navbar from "./Navbar.jsx";
+import Login from "./Login.jsx";
 
 
 const App = () => {
   return(
     <div>
-      <p>hello</p>
+      <Login/>
     </div>
   )
 }
---
+++ client/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Login = () => {
+  return(
+    <div>
+      <p>Login page</p>
+    </div>
+  )
+}
+
+export default Login;
