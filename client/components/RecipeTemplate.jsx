b4f01d34fab996f02f2cc26a0be58f5767adb5c0
mkymn10 <mkymn10@g.ucla.edu>
2023-03-04 18:07:47 -0800

    finish main outline of recipe style
    Co-authored-by: Raymond Ferrer <nathanferrer29@yahoo.com>

--- client/components/RecipeTemplate.jsx
+++ client/components/RecipeTemplate.jsx
@@ -3,46 +3,48 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const RecipeTemplate = () => {
+  const deleteRecipe = () => {};
 
-  const deleteRecipe = () => {
-
-  }
-
-  const redirectToInfoPage = () => {
-
-  }
+  const redirectToInfoPage = () => {};
 
   return (
-      <div className='recipe-template'>
-        <div className='recipe-image'>
-          IMAGE
-        </div>
-        <div className='recipe-info'>
-          <p className='recipe-info-name'> Name </p>
-          <div className="recipe-info-macros-container">
-            <div className="regular-serving-macros">
-              <p className='serving-macros-title'> SERVING SIZE </p>
-              <p className='recipe-info-macros-amount'> CARB: </p>
-              <p className='recipe-info-macros-amount'> FAT: </p>
-              <p className='recipe-info-macros-amount'> PROTEIN: </p>
-              <p className='recipe-info-macros-amount'> KCAL: </p>
-            </div>
-            <div className="single-serving-macros">
-              <p className='serving-macros-title'> SERVING SIZE </p>
-              <p className='recipe-info-macros-amount'> CARB: </p>
-              <p className='recipe-info-macros-amount'> FAT: </p>
-              <p className='recipe-info-macros-amount'> PROTEIN: </p>
-              <p className='recipe-info-macros-amount'> KCAL: </p>
-            </div>
-          </div>
-        </div>
-        <div className="crud-buttons">
-          <button className="delete-btn" onClick={deleteRecipe}>
-            Delete
-          </button>
+    <div className="recipe-template">
+      <div className="recipe-image">IMAGE</div>
+      <div className="recipe-info">
+        <p className="recipe-info-name"> Name </p>
+        <div className="recipe-info-macros-container">
+          <p className="macros-summary"> Macros Summary </p>
+          <p className="macros-count-label">Per Recipe</p>
+          <p className="macros-count-label">Per Serving</p>
+
+          <p className="serving-macros-title"> SERVING SIZE: </p>
+          <p className="macros-count-title"> 5 </p>
+          <p className="macros-count-title"> 5 </p>
+
+          <p className="recipe-info-macros-amount"> CARB: </p>
+          <p className="recipe-info-macros-count">20</p>
+          <p className="recipe-info-macros-count">20</p>
+
+          <p className="recipe-info-macros-amount"> FAT: </p>
+          <p className="recipe-info-macros-count">20</p>
+          <p className="recipe-info-macros-count">20</p>
+
+          <p className="recipe-info-macros-amount"> PROTEIN: </p>
+          <p className="recipe-info-macros-count">20</p>
+          <p className="recipe-info-macros-count">20</p>
+
+          <p className="recipe-info-macros-amount"> KCAL: </p>
+          <p className="recipe-info-macros-count">20</p>
+          <p className="recipe-info-macros-count">20</p>
         </div>
       </div>
-  )
-}
+      <div className="crud-buttons">
+        <button className="delete-btn" onClick={deleteRecipe}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default RecipeTemplate;
