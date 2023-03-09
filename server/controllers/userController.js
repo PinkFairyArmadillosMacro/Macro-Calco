72518dda803a033f761703502b611d37c889f036
mkymn10 <mkymn10@g.ucla.edu>
2023-03-09 15:23:12 -0800

    fix login on backend
    Co-authored-by: David Kim <david.toyou.kim@gmail.com>

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,7 +33,9 @@ userController.verifyUser = async (req, res, next) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
-      return next('Invalid username or password in userController.verifyUser.');
+      res.locals.isLogged = false;
+      return next();
+      // return next('Invalid username or password in userController.verifyUser.');
     }
     // `findOne` returns a document/model instances, while
     // `find` returns a query object, which has no access to the methods
